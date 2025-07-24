37b1d10682d7c019b9e85eb6c6d476d10c9701b6
TitanForDev <titanfordev@gmail.com>
2025-07-24 16:35:35 +0800

    modify

--- lesson6/wantgoo.py
+++ lesson6/wantgoo.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import twstock
+
 from crawl4ai import (AsyncWebCrawler,
                       BrowserConfig,
                       CrawlerRunConfig,
@@ -116,19 +118,24 @@ async def get_stock_data(urls)-> list[dict]:
 
     return all_results
 
-if __name__ == '__main__':
-    urls = [
-        "https://www.wantgoo.com/stock/2330/technical-chart",
-        "https://www.wantgoo.com/stock/2317/technical-chart",
-        "https://www.wantgoo.com/stock/2454/technical-chart",
-        "https://www.wantgoo.com/stock/2303/technical-chart",
-        "https://www.wantgoo.com/stock/2412/technical-chart",
-        "https://www.wantgoo.com/stock/2884/technical-chart",
-        "https://www.wantgoo.com/stock/2881/technical-chart",
-        "https://www.wantgoo.com/stock/2308/technical-chart",
-        "https://www.wantgoo.com/stock/2337/technical-chart",
-        "https://www.wantgoo.com/stock/2882/technical-chart",
-    ]
-    reuslts:list[dict] = asyncio.run(get_stock_data(urls=urls))
-    for stock in reuslts:
-        print(stock)
+
+def get_stocks_with_twstock()->list[dict]:
+    # 取得所有股票清單
+    stocks = twstock.codes
+    
+    stock_list = []
+    for code, info in stocks.items():
+        stock_list.append({
+            'code': code,
+            'name': info.name,
+            'market': info.market,
+            'group': info.group
+        })
+
+    return_list = []
+    
+    for item in stock_list:
+        # 只找尋股票代碼第1位數為2的股票,只要4個字元
+        if item['code'].startswith('2') and len(item['code']) == 4:
+            return_list.append(item)
+    return return_list
